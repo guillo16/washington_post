1b36c21253a600f838b867ee5870d376576eb5aa
guillermo varela <guillovarela16@gmail.com>
2019-11-01 13:34:51 -0300

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
+  get 'categories/index'
+  get 'categories/show'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
