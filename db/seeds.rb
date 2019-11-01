60f25969afaf636d63cdc3d7ea5b092ad61821eb
guillermo varela <guillovarela16@gmail.com>
2019-11-01 20:15:42 -0300

    back end in process

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,9 +21,10 @@ us = Category.create(name: 'Us')
 puts 'creating articles'
 
 users = User.all.sample
-categories = Category.all.sample
 
 20.times do
+  categories = [ politics, sports, tech, world, us].sample
+
 Article.create!(title: 'Invitados de lujo, sorpresas y alta costura: la celebración de los 25 años de trayectoria de Claudio Cosano',
  subtitle:'El diseñador argentino realizó una muestra de sus vestidos más icónicos acompañada de un espectacular desfile con las creaciones que marcaron cada etapa de su carrera. Con la presencia de Susana Giménez y la familia Messi en primera fila, los momentos más importantes de una noche mágica',
  photo: 'https://static01.nyt.com/images/2019/11/01/multimedia/01AlvaradoES-1/merlin_152292138_777f0ef5-414f-457f-b1ae-590d108e9837-superJumbo.jpg?quality=90&auto=webp',
@@ -35,7 +36,8 @@ Article.create!(title: 'Invitados de lujo, sorpresas y alta costura: la celebrac
  Con una exposición de 21 vestidos que usaron reconocidas figuras de la industria del modelaje, las personas pudieron ser testigos de las creaciones que marcaron un antes y un después en la carrera de Claudio Cosano. Desde Mirtha Legrand, Susana Giménez, Nicole Neumann, la familia Messi, Liz Solari, Wanda Nara, Teté Coustarot, Teresa Calandra, Nequi Galloti, entre otras diosas argentinas que eligieron al diseñador argentino repetidas veces.',
  content_2:'"Estoy muy contento y feliz con lo que se logró. Una exposición y puesta en escena impresionante. Además, el honor de tener a Susana Giménez y a la familia Messi en primera fila. Un vestido que no me voy a olvidar nunca: el del millón de Susana", dijo a Infobae Claudio Cosano. Autodidacta, obsesivo por la moldería y encandilado por los brillos bordados a mano, Cosano logró instalar su ADN en el universo de la moda femenina. Y al igual que la marca Silkey, su extensa carrera dejó una huella en el mundo fashionista.',
  content_3:'Los primeros en estrenar la pasarela fueron el director de la marca, Mauricio Wachs, la directora artística Elizabeth Yelin y Susana Giménez quienes brindaron unas palabras de aliento y emoción su querido amigo Claudio Cosano. "Queremos crear una mística del cuidado de la mujer y el hombre. Nos sentimos orgullosos y estamos felices", dijo Mauricio Wachs.',
- category: categories)
+ category: categories,
+ user: users)
 end
 
 
