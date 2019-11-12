9f01178c2f4af9ffbc3f44cef9d078b3309eb24c
guillermo varela <guillovarela16@gmail.com>
2019-11-12 12:31:49 -0300

    style in progress

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,7 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @articles = Article.all.limit(20).order(created_at: :desc)
   end
 
   def new
